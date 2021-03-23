b00385e865030bb264b671926be767ff33a10313
sabols2k <dangducchau2000@gmail.com>
2021-03-23 12:48:04 +0700

    route + controller

---
+++ src/app/controllers/SiteController.js
@@ -0,0 +1,15 @@
+class SiteController{
+
+    //[GET] /news
+    index(req,res){
+        res.render('home');
+    }
+
+    // [GET] /news/:slug  // slug là 1 biến động
+    search(req, res){
+       res.render('search');
+    }
+}
+module.exports = new SiteController;
+
+const siteController = require('./SiteController')
---
+++ src/index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const path = require('path')
+const handlebars  = require('express-handlebars')
+const morgan = require('morgan')
+const app = express()
+const port = 3000
+
+
+const route = require('./routes');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+//middleware
+app.use(express.urlencoded({
+  extend: true
+})); //
+app.use(express.json()); //
+
+//HTTP logger
+// app.use(morgan('combined')); //gửi các request URL 
+
+//template engine
+app.engine('hbs', handlebars({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname,'resources\\views'));
+
+
+//route init
+route(app);
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
